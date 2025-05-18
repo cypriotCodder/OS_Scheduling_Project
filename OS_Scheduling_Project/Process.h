89811f4c30e9530b05293e5f5fda5652a63baa04
cypriotCodder <huraynedim@icloud.com>
2025-05-18 23:08:52 +0300

    Add getters and setters to Process class
    Updated the `Process` class in `Process.h` to include getter methods for member variables: `getPID()`, `getArrivalTime()`, `getBurstTime()`, `getRemainingTime()`, and `getPriority()`. Added a setter method `remainingUpdate(int delta)` for modifying `remaining_time`. Modified the constructor to initialize the `priority` member variable with a default value of 0. Simplified the destructor by removing an unnecessary line and added the implementation of `remainingUpdate` inline.

--- OS_Scheduling_Project/Process.h
+++ OS_Scheduling_Project/Process.h
@@ -6,6 +6,16 @@ public:
 	Process(int pid, int arrivalTime, int burstTime, int remainingTime, int priority=0);
 	~Process();
 
+	//getters
+	int getPID() const { return pid; }
+	int getArrivalTime() const { return arrival_time; }
+	int getBurstTime() const { return burst_time; }
+	int getRemainingTime() const { return remaining_time; }
+	int getPriority() const { return priority; }
+	
+
+	//setters
+	void remainingUpdate(int delta);
 
 
 private:
@@ -26,5 +36,9 @@ Process::Process(int pid, int arrivalTime, int burstTime, int remainingTime, int
 }
 
 Process::~Process()
+{
+}
+
+inline void Process::remainingUpdate(int delta)
 {
 }
