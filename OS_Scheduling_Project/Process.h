dc073ec3e747b169c8c8dd24b43fbd7b23be7391
cypriotCodder <huraynedim@icloud.com>
2025-05-18 22:34:40 +0300

    Refactor Process class constructor and member variables
    Updated the `Process` class to include a parameterized constructor that initializes `pid`, `arrival_time`, `burst_time`, `remaining_time`, and an optional `priority`. Removed the default constructor and added corresponding private member variables to enhance functionality and encapsulation.

--- OS_Scheduling_Project/Process.h
+++ OS_Scheduling_Project/Process.h
@@ -3,15 +3,26 @@
 class Process
 {
 public:
-	Process();
+	Process(int pid, int arrivalTime, int burstTime, int remainingTime, int priority=0);
 	~Process();
 
-private:
 
+
+private:
+	int pid;
+	int arrival_time;
+	int burst_time;
+	int priority;
+	int remaining_time;
 };
 
-Process::Process()
+Process::Process(int pid, int arrivalTime, int burstTime, int remainingTime, int priority = 0)
 {
+	this->pid = pid;
+	this->arrival_time = arrivalTime;
+	this->burst_time = burstTime;
+	this->remaining_time = remainingTime;
+	this->priority = priority;
 }
 
 Process::~Process()
