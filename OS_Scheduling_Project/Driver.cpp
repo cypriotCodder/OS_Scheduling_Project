9aa3203a2d319ce81096a65e93de66da156739ca
cypriotCodder <huraynedim@icloud.com>
2025-05-18 21:51:03 +0300

    birth of branch

--- OS_Scheduling_Project/Driver.cpp
+++ OS_Scheduling_Project/Driver.cpp
@@ -8,7 +8,7 @@ int main()
 	cout << "This is a simple OS Scheduling Project.\n";
 	cout << "This project will implement various scheduling algorithms.\n";
 	cout << "The algorithms are implemented in C++.\n";
-	
+
 
 	return 0;
 }
