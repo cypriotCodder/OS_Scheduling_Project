df2989ad2e691dd28696e203718ba5743cda7b2d
cypriotCodder <huraynedim@icloud.com>
2025-05-19 12:44:09 +0300

    Add Scheduler class and related files
    Updated project file to include Scheduler.cpp. Created
    Scheduler.h with necessary includes and defined the
    Scheduler class with constructor and destructor.
    Implemented empty definitions in Scheduler.cpp.

--- OS_Scheduling_Project/Scheduler.h
+++ OS_Scheduling_Project/Scheduler.h
@@ -1,1 +1,16 @@
 #pragma once
+#include <vector>
+#include "Process.h"
+#include "GanttChart.h"
+
+
+class Scheduler
+{
+public:
+	Scheduler(std::vector<Process> processes);
+	~Scheduler();
+
+private:
+
+};
+
