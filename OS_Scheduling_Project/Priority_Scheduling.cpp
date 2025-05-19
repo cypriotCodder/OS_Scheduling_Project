f95cf531b1209ed44b49069cd26c160513694a53
cypriotCodder <huraynedim@icloud.com>
2025-05-19 23:02:54 +0300

    Add priority scheduling functionality and refactor code
    Updated `Priority_Scheduling.cpp` to include necessary headers and added a new `run` method for processing a vector of `Process` objects. Implemented basic checks and sorting by priority. Added placeholder methods for average turnaround and waiting times. Improved formatting in `Priority_Scheduling.h` for better readability.

--- OS_Scheduling_Project/Priority_Scheduling.cpp
+++ OS_Scheduling_Project/Priority_Scheduling.cpp
@@ -1,4 +1,11 @@
 #include "Priority_Scheduling.h"
+#include <iostream>
+#include <vector> 
+#include "Process.h"
+#include <cassert>
+#include "GanttChart.h"
+
+using namespace std;
 
 Priority_Scheduling::Priority_Scheduling()
 {
@@ -6,4 +13,33 @@ Priority_Scheduling::Priority_Scheduling()
 
 Priority_Scheduling::~Priority_Scheduling()
 {
+}
+
+void Priority_Scheduling::run(std::vector<Process> waitingQ) const
+{
+	GanttChart ganttChart;							
+	float time = 0;
+	assert(!waitingQ.empty() && "Waiting queue is empty");
+	assert(time >= 0 && "Time must be non-negative");
+
+	// Sort the processes based on priority
+	while (!waitingQ.empty()) {
+		auto it = min_element(
+			waitingQ.begin(),
+			waitingQ.end(),
+			[](Process& a, Process& b) {
+				return a.getPriority() < b.getPriority();
+			}
+		);
+	}
+}
+
+float Priority_Scheduling::getAverageTurnaroundTime(std::vector<Process> processes) const
+{
+	return 0.0f;
+}
+
+float Priority_Scheduling::getAverageWaitingTime(std::vector<Process> processes) const
+{
+	return 0.0f;
 }
--- OS_Scheduling_Project/Priority_Scheduling.h
+++ OS_Scheduling_Project/Priority_Scheduling.h
@@ -8,7 +8,7 @@ class Priority_Scheduling
 public:
 	Priority_Scheduling();
 	~Priority_Scheduling();
-	void run(std::vector<Process> waitingQ) const;
+	void run(std::vector<Process> waitingQ)							const;
 	float getAverageTurnaroundTime(std::vector<Process> processes)	const;
 	float getAverageWaitingTime(std::vector<Process> processes)		const;
 private:
