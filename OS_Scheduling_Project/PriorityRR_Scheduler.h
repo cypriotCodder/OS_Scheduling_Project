e6c1cbed8a5dcef302924dda5be54af03a4335c8
cypriotCodder <huraynedim@icloud.com>
2025-05-19 23:17:35 +0300

    Add PriorityRR and RR Scheduler header files
    Updated project files to include new headers for PriorityRR_Scheduler and RR_Scheduler.
    Organized these files under the "Header Files" filter in the project structure.
    Both headers define their respective classes with constructors and destructors.

---
+++ OS_Scheduling_Project/PriorityRR_Scheduler.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <vector>
+#include "Process.h"
+#include <iostream>
+
+class PriorityRR_Scheduler
+{
+public:
+	PriorityRR_Scheduler();
+	~PriorityRR_Scheduler();
+
+private:
+
+};
---
+++ OS_Scheduling_Project/RR_Scheduler.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <vector>
+#include "Process.h"
+#include <iostream>
+
+class RR_Scheduler
+{
+public:
+	RR_Scheduler();
+	~RR_Scheduler();
+
+private:
+
+};
