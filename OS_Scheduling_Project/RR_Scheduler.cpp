d8d5c07fb80c0fff15f05a85d5e20518e0f6b232
cypriotCodder <huraynedim@icloud.com>
2025-05-20 12:42:51 +0300

    Improve documentation and readability in RR_Scheduler
    Added detailed Doxygen-style comments for the constructor, destructor, and methods in the `RR_Scheduler` class to enhance clarity. Reformatted the initialization of the `tQuantum` variable for consistency. Refactored the `run` method and related calculations for waiting and turnaround times, improving comments and assertions. Overall, these changes focus on enhancing code documentation and ensuring a consistent coding style throughout the file.

--- OS_Scheduling_Project/RR_Scheduler.cpp
+++ OS_Scheduling_Project/RR_Scheduler.cpp
@@ -1,6 +1,6 @@
 #include "RR_Scheduler.h"
 #include <iostream>
-#include <vector> 
+#include <vector>
 #include "Process.h"
 #include <cassert>
 #include "GanttChart.h"
@@ -8,98 +8,135 @@
 
 using namespace std;
 
+/**
+ * @brief Constructor for RR_Scheduler class.
+ *
+ * Initializes the time quantum for Round Robin scheduling.
+ *
+ * @param tQ Time quantum for Round Robin scheduling.
+ */
 RR_Scheduler::RR_Scheduler(float tQ)
 {
-	this->tQuantum= tQ; // Initialize the time this->tQuantum for Round Robin scheduling
+    this->tQuantum = tQ;
 }
 
+/**
+ * @brief Destructor for RR_Scheduler class.
+ */
 RR_Scheduler::~RR_Scheduler()
 {
+    // Destructor implementation, if needed.
 }
 
+/**
+ * @brief Executes the Round Robin scheduling algorithm.
+ *
+ * This method runs the Round Robin scheduling algorithm on the provided waiting queue
+ * of processes, using the specified time quantum.
+ *
+ * @param waitingQ Vector of processes in the waiting queue.
+ */
 void RR_Scheduler::run(std::vector<Process> waitingQ) const
 {
-	GanttChart ganttChart;
-	float time = 0;
-	assert(!waitingQ.empty() && "Waiting queue is empty");
-	
-	std::deque<Process> processQueue(waitingQ.begin(), waitingQ.end());	// Create a deque to hold the processes in the waiting queue
+    GanttChart ganttChart; // Gantt chart to visualize scheduling
 
-	// Run the processes in round-robin fashion
+    float time = 0; // Current time
+    assert(!waitingQ.empty() && "Waiting queue is empty");
+
+    std::deque<Process> processQueue(waitingQ.begin(), waitingQ.end()); // Deque to hold processes
+
+    // Run the processes in round-robin fashion
     while (!processQueue.empty()) {
-		assert(time >= 0 && "Time must be non-negative");
+        assert(time >= 0 && "Time must be non-negative");
 
-        // take the front process
+        // Take the front process
         Process p = processQueue.front();
         processQueue.pop_front();
 
-		if (p.getRemainingTime() > this->tQuantum) {
-			// Process will run for the time quantum
-			p.setStart(time);					// Set start time
-			p.setEnd(time + this->tQuantum);			// Set end time
-			time += this->tQuantum;
-			p.remainingTimeUpdate(this->tQuantum);
+        if (p.getRemainingTime() > this->tQuantum) {
+            // Process runs for the time quantum
+            p.setStart(time); // Set start time
+            p.setEnd(time + this->tQuantum); // Set end time
+            time += this->tQuantum;
+            p.remainingTimeUpdate(this->tQuantum);
 
-			//calculate waiting time and turnaround time
-			p.calculateWaitingTime(time);
-			p.calculateTurnaroundTime(time);
-			ganttChart.stamp(p);
-			processQueue.push_back(p);			// Add the process back to the queue
+            // Calculate waiting time and turnaround time
+            p.calculateWaitingTime(time);
+            p.calculateTurnaroundTime(time);
+            ganttChart.stamp(p);
+            processQueue.push_back(p); // Add process back to queue
 
-			// Print the process details
-			cout << "Running Process PID: " << p.getPID() << endl;
-			cout << "Start Time: " << p.getStart() << endl;
-			cout << "End Time: " << p.getEnd() << endl;
-			cout << "Waiting Time: " << p.getWaitingTime() << endl;
-			cout << "Turnaround Time: " << p.getTurnaroundTime() << "\n" << endl;
-		}
-		else {
-			// Process will finish its execution
-			p.setStart(time); // Set start time
-			p.setEnd(time + p.getRemainingTime());	// Set end time
-			time += p.getRemainingTime();
-			p.remainingTimeUpdate(0);
+            // Print process details
+            cout << "Running Process PID: " << p.getPID() << endl;
+            cout << "Start Time: " << p.getStart() << endl;
+            cout << "End Time: " << p.getEnd() << endl;
+            cout << "Waiting Time: " << p.getWaitingTime() << endl;
+            cout << "Turnaround Time: " << p.getTurnaroundTime() << "\n" << endl;
+        }
+        else {
+            // Process finishes execution
+            p.setStart(time); // Set start time
+            p.setEnd(time + p.getRemainingTime()); // Set end time
+            time += p.getRemainingTime();
+            p.remainingTimeUpdate(0);
 
-			//calculate waiting time and turnaround time
-			p.calculateWaitingTime(time);
-			p.calculateTurnaroundTime(time);
-			ganttChart.stamp(p);
+            // Calculate waiting time and turnaround time
+            p.calculateWaitingTime(time);
+            p.calculateTurnaroundTime(time);
+            ganttChart.stamp(p);
 
-			// Print the process details
-			cout << "Running Process PID: " << p.getPID() << endl;
-			cout << "Start Time: " << p.getStart() << endl;
-			cout << "End Time: " << p.getEnd() << endl;
-			cout << "Waiting Time: " << p.getWaitingTime() << endl;
-			cout << "Turnaround Time: " << p.getTurnaroundTime() << "\n" << endl;
-		}
-
+            // Print process details
+            cout << "Running Process PID: " << p.getPID() << endl;
+            cout << "Start Time: " << p.getStart() << endl;
+            cout << "End Time: " << p.getEnd() << endl;
+            cout << "Waiting Time: " << p.getWaitingTime() << endl;
+            cout << "Turnaround Time: " << p.getTurnaroundTime() << "\n" << endl;
+        }
     }
-	std::cout << "===================== ROUND ROBIN ALGORITHM =====================\n";
-	ganttChart.print();
+
+    // Print Gantt chart for visualization
+    std::cout << "===================== ROUND ROBIN ALGORITHM =====================\n";
+    ganttChart.print();
 }
 
+/**
+ * @brief Calculates the average turnaround time for a list of processes.
+ *
+ * @param processes Vector of processes for which average turnaround time is calculated.
+ * @return Average turnaround time.
+ */
 float RR_Scheduler::getAverageTurnaroundTime(std::vector<Process> processes) const
 {
-	// Calculate the average turnaround time
-	float totalTurnaroundTime = 0.0f;
+    float totalTurnaroundTime = 0.0f;
 
-	assert(!processes.empty() && "Process list is empty");
-	assert(totalTurnaroundTime >= 0 && "Total turnaround time must be non-negative");
-	for (const auto& process : processes) {
-		totalTurnaroundTime += process.getTurnaroundTime();
-	}
-	return totalTurnaroundTime /= processes.size();
+    assert(!processes.empty() && "Process list is empty");
+    assert(totalTurnaroundTime >= 0 && "Total turnaround time must be non-negative");
+
+    // Calculate total turnaround time
+    for (const auto& process : processes) {
+        totalTurnaroundTime += process.getTurnaroundTime();
+    }
+
+    return totalTurnaroundTime / processes.size(); // Calculate and return average
 }
 
+/**
+ * @brief Calculates the average waiting time for a list of processes.
+ *
+ * @param processes Vector of processes for which average waiting time is calculated.
+ * @return Average waiting time.
+ */
 float RR_Scheduler::getAverageWaitingTime(std::vector<Process> processes) const
 {
-	// Calculate the average waiting time
-	float totalWaitingTime = 0.0f;
+    float totalWaitingTime = 0.0f;
 
-	assert(!processes.empty() && "Process list is empty");
-	assert(totalWaitingTime >= 0 && "Total turnaround time must be non-negative");
-	for (const auto& process : processes) {
-		totalWaitingTime += process.getWaitingTime();
-	}
-	return totalWaitingTime /= processes.size();
+    assert(!processes.empty() && "Process list is empty");
+    assert(totalWaitingTime >= 0 && "Total waiting time must be non-negative");
+
+    // Calculate total waiting time
+    for (const auto& process : processes) {
+        totalWaitingTime += process.getWaitingTime();
+    }
+
+    return totalWaitingTime / processes.size(); // Calculate and return average
 }
