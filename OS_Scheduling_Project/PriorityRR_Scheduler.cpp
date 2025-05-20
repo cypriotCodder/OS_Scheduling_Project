4d627d4e89e9647c79978571b47675b2e4e3de80
cypriotCodder <huraynedim@icloud.com>
2025-05-20 12:04:25 +0300

    Add PriorityRR_Scheduler implementation and project updates
    - Included `PriorityRR_Scheduler.cpp` in the project files.
    - Updated project filters to recognize the new source file.
    - Modified constructor and destructor in `PriorityRR_Scheduler.h` to use inline definitions.
    - Created `PriorityRR_Scheduler.cpp` with method stubs for `run`, `getAverageTurnaroundTime`, and `getAverageWaitingTime`.

---
+++ OS_Scheduling_Project/PriorityRR_Scheduler.cpp
@@ -0,0 +1,19 @@
+#include "PriorityRR_Scheduler.h"
+#include <vector>
+#include "Process.h"
+#include <iostream>
+#include <deque>
+
+void PriorityRR_Scheduler::run(std::vector<Process> waitingQ) const
+{
+}
+
+float PriorityRR_Scheduler::getAverageTurnaroundTime(std::vector<Process> processes) const
+{
+	return 0.0f;
+}
+
+float PriorityRR_Scheduler::getAverageWaitingTime(std::vector<Process> processes) const
+{
+	return 0.0f;
+}
--- OS_Scheduling_Project/PriorityRR_Scheduler.h
+++ OS_Scheduling_Project/PriorityRR_Scheduler.h
@@ -6,8 +6,8 @@
 class PriorityRR_Scheduler
 {
 public:
-	PriorityRR_Scheduler();
-	~PriorityRR_Scheduler();
+	PriorityRR_Scheduler() {};
+	~PriorityRR_Scheduler() {};
 
 	void run(std::vector<Process> waitingQ) const;
 	float getAverageTurnaroundTime(std::vector<Process> processes)	const;
